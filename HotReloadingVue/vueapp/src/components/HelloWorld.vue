85251768da0782d01383f0bef66a6a7150829ef3
keith.nicholas <keith.nicholas@gmail.com>
2019-05-30 22:03:21 +1200

    just a starter project demoing hot reloading and fetching from the api

---
+++ HotReloadingVue/vueapp/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{ results }}</h1>
+  
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import axios from 'axios';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  data() {
+    return {
+      results: []
+    }
+  }
+  async mounted() {
+    this.results = (await axios.get('/api/values')).data
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
